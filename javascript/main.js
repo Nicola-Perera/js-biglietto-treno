e0913f95048c111d0f09afe2c6cb16c13796c8ce
Nicola-Perera <nicola.perera96@mail.com>
2021-10-11 16:18:04 +0200

    add prompts from the user with his info

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,18 @@
 // Programma per calcolare il prezzo di un biglietto del treno
+
+// prezzo e sconto
 const price_per_km = 0.21;
 const discount_under18 = 0.8;
 const discount_over65 = 0.6;
+
+// interazione con l'utente
+alert('Benvenuto! Creiamo insieme il biglietto adatto a te');
+
+// richiesta percorso all'utente
+let trip_distance = parseInt(prompt('Quanti chilometri devi percorrere?'));
+console.log(trip_distance);
+
+// richiesta età dell'utente
+let user_age = parseInt(prompt('Quanti anni hai?'));
+console.log(user_age);
+
