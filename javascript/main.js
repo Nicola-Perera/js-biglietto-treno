e081afdfbacc947f2575fb577ab25d213597cdcf
Nicola-Perera <nicola.perera96@mail.com>
2021-10-11 17:13:17 +0200

    added text to html with the final price
    fixed parseInt with parseFloat

--- javascript/main.js
+++ javascript/main.js
@@ -10,15 +10,15 @@ alert('Benvenuto! Creiamo insieme il biglietto adatto a te');
 
 // richiesta percorso all'utente
 let trip_distance = parseFloat(prompt('Quanti chilometri devi percorrere?'));
-console.log(trip_distance);
+
 
 // richiesta età dell'utente
 let user_age = parseFloat(prompt('Quanti anni hai?'));
-console.log(user_age);
+
 
 // creazione biglietto
 let full_price = trip_distance * price_per_km;
-console.log(full_price);
+
 
 // applicazione sconti
 let final_price;
@@ -32,7 +32,7 @@ else {
     final_price = full_price;
 }
 
-
-console.log((final_price).toFixed(2));
+// stampa biglietto
+document.getElementById('ticket_price').innerHTML = final_price.toFixed(2) + '€';
 
 
